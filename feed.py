72a78b96ad8ff850acb2af64a3128ced11fbad07
ikkibr <ikkibr@8f8b154b-7c4a-0410-9065-a97ea2a65816>
2008-04-12 14:29:30 +0000

    Did some code cleaning in feed.py Added an auxiliary variable to feed.py to fix a bug that was happening in appserver.com. Removed unnecessary {{ current_type }} from templates/index.html

--- feed.py
+++ feed.py
@@ -4,25 +4,25 @@ from google.appengine.ext import db
 from google.appengine.ext.webapp import template
 from paste import Paste
 
-class Feed(webapp.RequestHandler):
+class Feed(webapp.RequestHandler):   
     def get_feeds(self,user):
         
-        if user:
-            user_feeds = db.GqlQuery("SELECT * FROM Paste WHERE name = :1 ORDER BY date DESC",user)
-        else:
-            user_feeds = db.GqlQuery("SELECT * FROM Paste ORDER BY date DESC")
-
-        return user_feeds.fetch(10)
+        user_feeds = db.GqlQuery("SELECT * FROM Paste WHERE name = :1 ORDER BY date DESC",user)
+        #need to check if paste is public. 
+			
+	real_feeds = user_feeds.fetch(10)
+        return real_feeds
 
     def get(self):
         user = self.request.path.split('\n')
         user = user[0].split('f/')[1]
         aux = {}
+        aux['user'] = user
         aux['feeds'] = self.get_feeds(user=user)
 
-        return self.response.out.write(template.render("templates/feed.xml", aux))
+        return self.response.out.write(template.render( "templates/feed.xml" , aux))
         
-
+        
 def main():
     application = webapp.WSGIApplication([('/.*', Feed)], debug=True)
     wsgiref.handlers.CGIHandler().run(application)
